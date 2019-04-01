810ef64a210ae6e461dd2696895e7b65e6205c4a
1339395263@qq.com <zhx13376727028>
2019-04-01 10:17:53 +0800

    去除统计图添加轮询

--- static/js/member.js
+++ static/js/member.js
@@ -109,45 +109,45 @@ layui.define(['echarts','jqbind'], function(exports) {
                 var sexobj = echarts.init(document.getElementById('sex'));
                 sexobj.setOption(option);
 
-                var article = echarts.init(document.getElementById('article'));
-                var option = {
-                    title: {
-                        text: '最近七天发布文章数',
-                        subtext: '文章数',
-                        x: 'center'
-                    },
-                    color: ['#3398DB'],
-                    tooltip: {
-                        trigger: 'axis',
-                        axisPointer: { // 坐标轴指示器，坐标轴触发有效
-                            type: 'shadow' // 默认为直线，可选为：'line' | 'shadow'
-                        }
-                    },
-                    grid: {
-                        left: '3%',
-                        right: '4%',
-                        bottom: '3%',
-                        containLabel: true
-                    },
-                    xAxis: [{
-                        type: 'category',
-                        data: articleName,
-                        axisTick: {
-                            alignWithLabel: true
-                        }
-                    }],
-                    yAxis: [{
-                        type: 'value'
-                    }],
-                    series: [{
-                        name: '活跃度',
-                        type: 'bar',
-                        barWidth: '60%',
-                        data: articleData
-                    }]
-                };
-                // 使用刚指定的配置项和数据显示图表。
-                article.setOption(option);
+                // var article = echarts.init(document.getElementById('article'));
+                // var option = {
+                //     title: {
+                //         text: '最近七天发布文章数',
+                //         subtext: '文章数',
+                //         x: 'center'
+                //     },
+                //     color: ['#3398DB'],
+                //     tooltip: {
+                //         trigger: 'axis',
+                //         axisPointer: { // 坐标轴指示器，坐标轴触发有效
+                //             type: 'shadow' // 默认为直线，可选为：'line' | 'shadow'
+                //         }
+                //     },
+                //     grid: {
+                //         left: '3%',
+                //         right: '4%',
+                //         bottom: '3%',
+                //         containLabel: true
+                //     },
+                //     xAxis: [{
+                //         type: 'category',
+                //         data: articleName,
+                //         axisTick: {
+                //             alignWithLabel: true
+                //         }
+                //     }],
+                //     yAxis: [{
+                //         type: 'value'
+                //     }],
+                //     series: [{
+                //         name: '活跃度',
+                //         type: 'bar',
+                //         barWidth: '60%',
+                //         data: articleData
+                //     }]
+                // };
+                // // 使用刚指定的配置项和数据显示图表。
+                // article.setOption(option);
 
             }
 
